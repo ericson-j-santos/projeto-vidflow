23e7f884045d27dffbe7e9bb3bb86c7cfde3c3f9
Ericson <ericson.takay@gmail.com>
2024-08-09 14:32:18 -0300

    Fim - Async/Await; Inicio - Tratando erros com try...catch

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const containerVideos = document.querySelector('.videos__container')
 
-const api = fetch('http://localhost:3000/videos')
-    .then(res => res.json())
-    .then((videos) =>
-        videos.forEach((video) => {
-            containerVideos.innerHTML += `
-                <li class="videos__item">
-                <iframe width="100%" height="62%" src="${video.url}"
-                    title="${video.titulo}" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
-                <div class="descricao-video">
-                    <img class="img-canal" src="${video.imagem}" alt="Logo do canal'></img>
-                    <h3 class="titulo-video">${video.titulo}</h3>
-                    <p class="titulo-canal">${video.descricao}</p>
-                </div>
-            </li>
-            `;
-        })
-    )
-    .catch(error => {
-        containerVideos.innerHTML = `<p style="color: red;">Houve um erro ao carregar os vídeos: ${error}. Por favor, tente novamente mais tarde.</p>`;
-    });
+async function buscarEMostrarVideos() {
+    const busca = await fetch('http://localhost:3000/videos');
+    const videos = await busca.json()
+
+    videos.forEach((video) => {
+        containerVideos.innerHTML += `
+                    <li class="videos__item">
+                    <iframe width="100%" height="62%" src="${video.url}"
+                        title="${video.titulo}" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo do canal'></img>
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+                `;
+    })
+}
+
+buscarEMostrarVideos()
