5726e59df99db98d009305848a9bc69553fc10c1
Ericson <ericson.takay@gmail.com>
2024-08-09 14:18:23 -0300

    Fim - Async/Await; Inicio - Tratando erros com try..catch;

--- script.js
+++ script.js
@@ -5,19 +5,20 @@ const api = fetch('http://localhost:3000/videos')
     .then((videos) =>
         videos.forEach((video) => {
             containerVideos.innerHTML += `
-            <li class="videos__item">
-                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                <li class="videos__item">
+                <iframe width="100%" height="62%" src="${video.url}"
+                    title="${video.titulo}" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
                 <div class="descricao-video">
                     <img class="img-canal" src="${video.imagem}" alt="Logo do canal'></img>
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
-                    </div>
+                </div>
             </li>
             `;
         })
     )
-    .catch((error) => {
-        containerVideos.innerHTML = `
-        <p>Houve um erro ao carregar os vídeos: ${error}</p>;
-        `
-    })
+    .catch(error => {
+        containerVideos.innerHTML = `<p style="color: red;">Houve um erro ao carregar os vídeos: ${error}. Por favor, tente novamente mais tarde.</p>`;
+    });
