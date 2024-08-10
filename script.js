81b89fc44b4eaa2cd80908f58ae510690a3f1999
Ericson <ericson.takay@gmail.com>
2024-08-10 12:46:18 -0300

    Fim - tratando erros com try...catch; Inicio - 04.Criando filtros de exibição - 02. Vinculando evento à barra de pesquisa.

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-const containerVideos = document.querySelector('.videos__container')
+const containerVideos = document.querySelector('.videos__container') // Seleciona o container de vídeos na tela 
 
 async function buscarEMostrarVideos() {
-    const busca = await fetch('http://localhost:3000/videos');
-    const videos = await busca.json()
+    try { // Tenta fazer a requisição fetch para a API de vídeos e exibir os vídeos na tela 
+        const busca = await fetch('http://localhost:3000/videos'); // Faz a requisição fetch para a API de vídeos 
+        const videos = await busca.json() // Converte a resposta da API para JSON
 
-    videos.forEach((video) => {
-        containerVideos.innerHTML += `
-                    <li class="videos__item">
-                    <iframe width="100%" height="62%" src="${video.url}"
-                        title="${video.titulo}" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
-                    <div class="descricao-video">
-                        <img class="img-canal" src="${video.imagem}" alt="Logo do canal'></img>
-                        <h3 class="titulo-video">${video.titulo}</h3>
-                        <p class="titulo-canal">${video.descricao}</p>
-                    </div>
-                </li>
-                `;
-    })
+        videos.forEach((video) => {  // Para cada vídeo retornado pela API, exibe o vídeo na tela.
+            // Por exemplo, se for uma regra da sua aplicação que uma categoria nunca deve vir vazia, se a categoria for vazia, usaremos o throw new Error(). Entre os parênteses, exibiremos uma mensagem, que é "Vídeo sem categoria".
+            if (video.categoria == "") {
+                throw new Error("Vídeo não tem categoria")
+            }
+            containerVideos.innerHTML += ` 
+                        <li class="videos__item">
+                            <iframe width="100%" height="62%" src="${video.url}"
+                            title="${video.titulo}" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                        <div class="descricao-video">
+                            <img class="img-canal" src="${video.imagem}" alt="Logo do canal'></img>
+                            <h3 class="titulo-video">${video.titulo}</h3>
+                            <p class="titulo-canal">${video.descricao}</p>
+                        </div>
+                    </li>
+                    `; // Adiciona o vídeo na tela 
+        }) // Fim do forEach
+    } catch (error) { // Se der erro na requisição fetch, exibe o erro no console do navegador 
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`
+    }
+    // finally { // Seja qual for o resultado da requisição fetch, exibe a mensagem de carregamento dos vídeos na tela 
+    //     //alert('Carregando vídeos...')
+    //     containerVideos.innerHTML += `<p> Carregando vídeos... </p>`
+    // } // Fim do try...catch...finally
 }
 
 buscarEMostrarVideos()
