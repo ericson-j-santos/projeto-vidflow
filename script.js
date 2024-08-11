153dedf01d16ecb22faeb6acf4f75ef89c035c4f
Ericson <ericson.takay@gmail.com>
2024-08-11 15:20:02 -0300

    Fim - Addicionando filtro de categorias;; Inicio - Criando a lógica de filtragem;

--- script.js
+++ script.js
@@ -1,61 +1,84 @@
-const containerVideos = document.querySelector('.videos__container') // Seleciona o container de vídeos na tela 
+const containerVideos = document.querySelector('.videos__container') // Seleciona o container de vídeos na tela
 
+// fetch() busca em uma API informações e retorna uma objeto Promise - função assíncrona.
+
+// Promessa de quando os dados vierem, algo será feito com base neles
+
+// keyword async serve para escrever codigo assíncrono de forma mais síncrona. Nesse caso o async determina que essa função se irá funcionar se o await for resolvido
 async function buscarEMostrarVideos() {
+
+    // Tratamento de erros
     try { // Tenta fazer a requisição fetch para a API de vídeos e exibir os vídeos na tela 
+
         const busca = await fetch('http://localhost:3000/videos'); // Faz a requisição fetch para a API de vídeos 
         const videos = await busca.json() // Converte a resposta da API para JSON
 
+        // Quando a promise retorna "fulfilled" será necessário fazer algo com o operador .then 'Então...'
+
         videos.forEach((video) => {  // Para cada vídeo retornado pela API, exibe o vídeo na tela.
             // Por exemplo, se for uma regra da sua aplicação que uma categoria nunca deve vir vazia, se a categoria for vazia, usaremos o throw new Error(). Entre os parênteses, exibiremos uma mensagem, que é "Vídeo sem categoria".
+
             if (video.categoria == "") {
+
                 throw new Error("Vídeo não tem categoria")
+
             }
-            containerVideos.innerHTML += ` 
-                        <li class="videos__item">
-                            <iframe width="100%" height="62%" src="${video.url}"
-                            title="${video.titulo}" frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen></iframe>
-                        <div class="descricao-video">
-                            <img class="img-canal" src="${video.imagem}" alt="Logo do canal'></img>
-                            <h3 class="titulo-video">${video.titulo}</h3>
-                            <p class="titulo-canal">${video.descricao}</p>
-                        </div>
-                    </li>
-                    `; // Adiciona o vídeo na tela 
+
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+                    <iframe src="${video.url}" title"=${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                    <img class="img-canal" src="${video.imagem}" alt="logo do Canal">
+                    <h3 class="titulo-video">${video.titulo}</h3>
+                    <p class="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
+                    </div>
+                </li>
+            `; // Adiciona o vídeo na tela 
         }) // Fim do forEach
     } catch (error) { // Se der erro na requisição fetch, exibe o erro no console do navegador 
         containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`
     }
-    // finally { // Seja qual for o resultado da requisição fetch, exibe a mensagem de carregamento dos vídeos na tela 
-    //     //alert('Carregando vídeos...')
-    //     containerVideos.innerHTML += `<p> Carregando vídeos... </p>`
-    // } // Fim do try...catch...finally
 }
 
-buscarEMostrarVideos()
+buscarEMostrarVideos();
 
-const barraDePesquisa = document.querySelector(".pesquisar__input") // Seleciona a barra de pesquisa na tela
 
-barraDePesquisa.addEventListener("input", filtraPesquisa) // Adiciona um evento de input na barra de pesquisa
+const barraDePesquisa = document.querySelector('.pesquisar__input'); // Seleciona a barra de pesquisa na tela
 
-function filtrarPesquisa() {
-    const videos = document.querySelectorAll(".videos__item") // Seleciona todos os vídeos exibidos na tela
-    if (barraDePesquisa.value != "")
+barraDePesquisa.addEventListener('input', filtrarPesquisa); // Adiciona um evento de input na barra de pesquisa
+
+
+function filtrarPesquisa() { // Função que filtra a pesquisa na barra de pesquisa
+    const videos = document.querySelectorAll(".videos__item"); // Seleciona todos os vídeos exibidos na tela
+
+    if (barraDePesquisa.value != "") { // Se o valor da barra de pesquisa não estiver vazio
+        //debugger
+        for (let video of videos) { // Para cada vídeo exibido na tela
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase(); // Pega o título do vídeo e converte para minúsculo
+            let valorFiltro = barraDePesquisa.value.toLowerCase(); // Pega o valor digitado na barra de pesquisa e converte para minúsculo
+
+            if (!titulo.includes(valorFiltro)) { // Se o título do vídeo não contém o valor digitado na barra de pesquisa
+                video.style.display = "none"; // Esconde o vídeo da tela aplicando o display none na propriedade style do vídeo
+            } else {
+                video.style.display = "block"; // Se o título do vídeo contém o valor digitado na barra de pesquisa, exibe o vídeo na tela aplicando o display block na propriedade style do vídeo
+            }
+        }
+    } else {
+        video.style.display = "block"; // Se o valor da barra de pesquisa estiver vazio, exibe todos os vídeos na tela aplicando o display block na propriedade style dos vídeos
+    }
 }
 
+const botaoCategoria = document.querySelectorAll('.superior__item') // Seleciona todos os botões de categoria na tela
 
-// barraDePesquisa.addEventListener('input', async (evento) => { // Adiciona um evento de input na barra de pesquisa
-//     const termoDeBusca = evento.target.value // Pega o valor digitado na barra de pesquisa
-//     containerVideos.innerHTML = '' // Limpa o container de vídeos na tela
-//     await buscarEMostrarVideos() // Busca e exibe os vídeos na tela
-//     const videos = document.querySelectorAll('.videos__item') // Seleciona todos os vídeos exibidos na tela
-//     videos.forEach((video) => { // Para cada vídeo exibido na tela
-//         const tituloDoVideo = video.querySelector('.titulo-video').textContent // Pega o título do vídeo
-//         const descricaoDoVideo = video.querySelector('.titulo-canal').textContent // Pega a descrição do vídeo
-//         if (!tituloDoVideo.includes(termoDeBusca) && !descricaoDoVideo.includes(termoDeBusca)) { // Se o termo de busca não estiver no título e na descrição do vídeo
-//             video.style.display = 'none' // Esconde o vídeo da tela
-//         } // Fim do if
-//     }) // Fim do forEach
+botaoCategoria.forEach((botao) => { // Para cada botão de categoria na tela 
+    let nomeCategoria = botao.getAttribute('name') // Pega o nome da categoria do botão 
+    botao.addEventListener('click', () => filtrarPorCategoria(nomeCategoria)) // Adiciona um evento de clique no botão e chama a função filtrarPorCategoria passando o nome da categoria como parâmetro
+})
 
-// })
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll('.videos__item') // Seleciona todos os vídeos exibidos na tela
+    for (let video of videos) {
+
+    }
+}
